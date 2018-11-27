418b5843425218bc203ac303a319aa0211af5272
moskanka <moskanka@yandex.ru>
2018-11-27 13:15:50 +0300

    init commit

---
+++ create_mask.cpp
@@ -0,0 +1,119 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/imgcodecs/imgcodecs.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+Mat src, img1, mask, final;
+
+Point point;
+vector<Point> pts;
+int drag = 0;
+int var = 0;
+int flag = 0;
+
+void mouseHandler(int, int, int, int, void*);
+
+void mouseHandler(int event, int x, int y, int, void*)
+{
+    if (event == EVENT_LBUTTONDOWN && !drag)
+    {
+        if (flag == 0)
+        {
+            if (var == 0)
+                img1 = src.clone();
+            point = Point(x, y);
+            circle(img1, point, 2, Scalar(0, 0, 255), -1, 8, 0);
+            pts.push_back(point);
+            var++;
+            drag  = 1;
+
+            if (var > 1)
+                line(img1,pts[var-2], point, Scalar(0, 0, 255), 2, 8, 0);
+
+            imshow("Source", img1);
+        }
+    }
+
+    if (event == EVENT_LBUTTONUP && drag)
+    {
+        imshow("Source", img1);
+        drag = 0;
+    }
+
+    if (event == EVENT_RBUTTONDOWN)
+    {
+        flag = 1;
+        img1 = src.clone();
+
+        if (var != 0)
+        {
+            polylines(img1, pts, 1, Scalar(0,0,0), 2, 8, 0);
+        }
+
+        imshow("Source", img1);
+    }
+
+    if (event == EVENT_RBUTTONUP)
+    {
+        flag = var;
+        final = Mat::zeros(src.size(), CV_8UC3);
+        mask = Mat::zeros(src.size(), CV_8U);
+
+        vector<vector<Point> > vpts;
+        vpts.push_back(pts);
+        fillPoly(mask, vpts, Scalar(255, 255, 255), 8, 0);
+        bitwise_and(src, src, final, mask);
+        imshow("Mask", mask);
+        imwrite("/home/anne/programming/openCV/VideoCapturing/mask.jpg", mask);
+        imshow("Result", final);
+        imshow("Source", img1);
+    }
+
+    if (event == EVENT_MBUTTONDOWN)
+    {
+        pts.clear();
+        var = 0;
+        drag = 0;
+        flag = 0;
+        imshow("Source", src);
+    }
+}
+
+
+int main(int argc, char **argv)
+{
+    cout << "left mouse button - set a point to create mask shape" << endl
+         << "right mouse button - create mask from points" << endl
+         << "middle mouse button - reset" << endl;
+
+    VideoCapture inputVideo(0);
+    if (!inputVideo.isOpened())  // check if we succeeded
+    {
+        std::cout << "Couldn't connect to camera" << std::endl;
+        return -1;
+    }
+
+    int width = (int) inputVideo.get(CAP_PROP_FRAME_WIDTH);
+    int height = (int) inputVideo.get(CAP_PROP_FRAME_HEIGHT);
+    Size S = Size(width, height); // set size of input frames
+
+    //src = imread("/home/anne/programming/openCV/test/1.jpg", IMREAD_COLOR);
+
+    inputVideo >> src;
+    if (src.empty()) return 0;
+
+    namedWindow("Source", WINDOW_AUTOSIZE);
+    setMouseCallback("Source", mouseHandler, NULL);
+    imshow("Source", src);
+    waitKey(0);
+
+//    Mat newimage;
+//    newimage = imread("/home/anne/programming/openCV/VideoCapturing/12.jpg", IMREAD_COLOR);
+//    src.copyTo(newimage, mask);
+//    imwrite("/home/anne/programming/openCV/VideoCapturing/new.jpg", newimage);
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <string>
+//#include <vector>
+#include <time.h>
+//#include <cv.h>
+
+#include "opencv2/highgui/highgui.hpp"
+//#include <opencv2/core/core.hpp>
+
+//#include "opencv2/videoio.hpp"
+#include "opencv2/opencv.hpp"
+//#include <opencv2/videoio.hpp>
+//#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+
+
+int main(int, char**)
+{
+    const std::string outputFile = "/home/anne/programming/openCV/VideoCapturing/photos/cap.avi";
+    const std::string outputFileWithMask = "/home/anne/programming/openCV/VideoCapturing/photos/cap_withmask.avi";
+
+    VideoCapture inputVideo(0); // open camera
+    if (!inputVideo.isOpened())  // check if we succeeded
+    {
+        std::cout << "Couldn't connect to camera" << std::endl;
+        return -1;
+    }
+
+    int ex = static_cast<int>(inputVideo.get(CAP_PROP_FOURCC)); // Get Codec Type - Int form
+    int width = (int) inputVideo.get(CAP_PROP_FRAME_WIDTH);
+    int height = (int) inputVideo.get(CAP_PROP_FRAME_HEIGHT);
+    Size S = Size(width, height); // set size of input frames
+
+    VideoWriter outputVideo; // output stream to file (captured video)
+    outputVideo.open(outputFile, ex, inputVideo.get(CAP_PROP_FPS), S, true);
+    if (!outputVideo.isOpened())
+    {
+        std::cout  << "Could not open the output videofile for write: " << outputFile << std::endl;
+        return -1;
+    }
+
+    VideoWriter outputVideoWithMask; // output stream to file (masked video)
+    outputVideoWithMask.open(outputFileWithMask, ex, inputVideo.get(CAP_PROP_FPS), S, true);
+    if (!outputVideoWithMask.isOpened())
+    {
+        std::cout  << "Could not open the output videofile for write with mask: " << outputFile << std::endl;
+        return -1;
+    }
+
+    Mat mask;
+    mask = imread("/home/anne/programming/openCV/VideoCapturing/mask.jpg", IMREAD_COLOR);
+
+    Mat prevFrame;
+    Mat thisFrame;
+    Mat maskedFrame;
+
+    std::string name = "";
+
+    time_t rawtime0;
+    time(&rawtime0);
+    struct tm * prev_timeinfo = localtime(&rawtime0);
+    std::string timeinfo_str = "";
+    std::string prev_timeinfo_str = std::string(asctime(prev_timeinfo));;
+    int i = 0;
+
+    // надо добавить проверку свободного места на диске
+    for (;;)
+    {
+        inputVideo >> thisFrame; // get a new frame from camera
+        if (thisFrame.empty()) break; // check if videostream ended, then break
+
+        namedWindow("frame", WINDOW_AUTOSIZE);
+        namedWindow("maskedFrame", WINDOW_AUTOSIZE);
+
+
+
+        //outputVideo.write(thisFrame); //save or
+        //outputVideo << frame;
+
+        time_t rawtime;
+        time(&rawtime);
+        struct tm * timeinfo;
+        time(&rawtime);
+        timeinfo = localtime(&rawtime);
+        timeinfo_str = std::string(asctime(timeinfo));
+        name = timeinfo_str.substr(0, timeinfo_str.size()-1);
+        if (prev_timeinfo_str == timeinfo_str)
+            i++;
+        else
+            i = 0;
+        name = name + "-" + std::to_string(i) + ".jpg"; // Tue Nov 27 12:36:53 2018-1
+//        std::cout << "-" << name << "-" << std::endl;
+
+        imshow("frame", thisFrame);
+        outputVideo << thisFrame;
+
+        Mat savingFrame;
+        thisFrame.copyTo(savingFrame);
+
+        if (prevFrame.empty())
+            thisFrame.copyTo(prevFrame);
+
+        prevFrame.copyTo(thisFrame, mask);
+        try
+        {
+            bool result = false;
+            Mat res;
+            absdiff(thisFrame, prevFrame, res);
+            if (!res.empty())
+            {
+                putText(savingFrame, // frame
+                        timeinfo_str.substr(0, timeinfo_str.size()-1), // string
+                        Point(100,100),
+                        FONT_HERSHEY_DUPLEX,
+                        0.8, // thickness
+                        Scalar(0,145,145) // color
+                );
+                result = imwrite("/home/anne/programming/openCV/VideoCapturing/photos/" + name, savingFrame);
+            }
+//            std::cout << result << "\n";
+        }
+        catch (const cv::Exception& ex)
+        {
+            fprintf(stderr, "Exception converting image to PNG format: %s\n", ex.what());
+        }
+        thisFrame.copyTo(prevFrame);
+
+        putText(thisFrame, // frame
+                timeinfo_str.substr(0, timeinfo_str.size()-1), // string
+                Point(100,100),
+                FONT_HERSHEY_DUPLEX,
+                0.8, // thickness
+                Scalar(0,145,145) // color
+        );
+        prev_timeinfo_str = timeinfo_str;
+
+        imshow("maskedFrame", thisFrame);
+        outputVideoWithMask << thisFrame;
+
+
+
+        waitKey(30);
+    }
+    return 0;
+}
